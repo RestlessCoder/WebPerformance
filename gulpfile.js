ec826801ddbf0c20f37aa87cac6e67770b23cfdd
RestlessCoder <nick.limca@hotmail.com>
2017-05-10 02:34:41 +0100

    Optimize & Resize Images

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 
 var gulp = require('gulp'); // Gulp
 var minifyCSS = require('gulp-clean-css'); // CSS
+var imagemin = require('gulp-imagemin'); // Optimize Image
 
+// CSS Task
+// Minify
 gulp.task('minify-css', function(){
   gulp.src('./css/*.css')
     .pipe(minifyCSS())
@@ -10,3 +13,18 @@ gulp.task('minify-css', function(){
 
 });
 
+// Image Task
+// Compress
+gulp.task('compress-image', function(){
+	gulp.src([
+			  'views/images/*',
+			  'img/*'
+			 ])
+		.pipe(imagemin([
+    		  imagemin.gifsicle({interlaced: true}),
+    		  imagemin.jpegtran({progressive: true}),
+    		  imagemin.optipng({optimizationLevel: 5}),
+    		  imagemin.svgo({plugins: [{removeViewBox: true}]})
+		]))
+		.pipe(gulp.dest('dist/images'))
+});
