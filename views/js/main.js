c77c4eb3df45ef4f755571221da28bd8fc6ed9fa
RestlessCoder <nick.limca@hotmail.com>
2017-05-13 13:21:24 +0100

    Reduce the time to resize pizza < 5 ms by using slider

--- views/js/main.js
+++ views/js/main.js
@@ -445,10 +445,15 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    // Changing 'querySelectorAll' to getElementsByClassName to reduce time to resize pizza
+    var randomPizzas =  document.getElementsByClassName("randomPizzaContainer");
+
+    // Moving these variabled declarations outside loop drastically reduces time to resize pizzas
+    var i = 0;
+    var dx = determineDx(randomPizzas[i], size);
+    var newwidth = (randomPizzas[i].offsetWidth + dx) + 'px';
+    for (i = 0; i < randomPizzas.length; i++) {
+      randomPizzas[i].style.width = newwidth;
     }
   }
 
